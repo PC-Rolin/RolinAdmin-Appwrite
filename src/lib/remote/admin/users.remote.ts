3bea0262284aee1f29ab6dae7c68196fc90a2489
Niels Krijnen <niels.krijnen@icloud.com>
2025-10-08 20:21:00 +0200

    Upgrade `node-appwrite` dependency to v20.1.0 and remove unused `getRequestEvent` import.

--- src/lib/remote/admin/users.remote.ts
+++ src/lib/remote/admin/users.remote.ts
@@ -1,9 +1,7 @@
-import { getRequestEvent, query } from "$app/server";
+import { query } from "$app/server";
 import { createAdminClient } from "$lib/server/appwrite";
 
 export const list = query(async () => {
-  const event = getRequestEvent()
-  console.log(event)
   const { users } = createAdminClient()
 
   return users.list<Preferences>()
